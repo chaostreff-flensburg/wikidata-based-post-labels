7a3ce3860e35fed1027c584a0639f178eb9bc6bc
scammo <samuel.brinkmann@gmail.com>
2019-04-27 18:51:43 +0200

    fixed. More beauty

--- schulen.js
+++ schulen.js
@@ -1,45 +1,57 @@
-class SPARQLQueryDispatcher {
-	constructor( endpoint ) {
-		this.endpoint = endpoint;
-	}
-
-	query( sparqlQuery ) {
-		const fullUrl = this.endpoint + '?query=' + encodeURIComponent( sparqlQuery );
-		const headers = { 'Accept': 'application/sparql-results+json' };
-
-		return fetch( fullUrl, { headers } ).then( body => body.json() );
-	}
+function makeSPARQLQuery( endpointUrl, sparqlQuery, doneCallback ) {
+	var settings = {
+		headers: { Accept: 'application/sparql-results+json' },
+		data: { query: sparqlQuery }
+	};
+	return $.ajax( endpointUrl, settings ).then( doneCallback );
 }
 
-const endpointUrl = 'https://query.wikidata.org/sparql';
-const sparqlQuery = `#Schulen 30 km um Flensburg
+$.urlParam = function(name){
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results==null) {
+     return null;
+  }
+  return decodeURI(results[1]) || 0;
+}
 
-SELECT ?place ?placeLabel ?location
-WHERE
-{
-  # Flensburg coordinates
-  wd:Q3798 wdt:P625 ?flensburgLoc .
-  SERVICE wikibase:around {
-    ?place wdt:P625 ?location .
-    bd:serviceParam wikibase:center ?flensburgLoc .
-    bd:serviceParam wikibase:radius "30" .
-  } .
-  # Is an school
-  FILTER EXISTS { ?place wdt:P31 wd:Q159334 } .
-  SERVICE wikibase:label {
-    bd:serviceParam wikibase:language "de" .
-  } 
-}`;
+if($.urlParam('km')){    
+  var endpointUrl = 'https://query.wikidata.org/sparql',
+  sparqlQuery = "#Schulen 30 km um Flensburg\n" +
+  "\n" +
+  "SELECT ?place ?placeLabel ?location ?address ?schueler\n" +
+  "WHERE\n" +
+  "{\n" +
+  "  # Flensburg coordinates\n" +
+  "  wd:Q3798 wdt:P625 ?flensburgLoc .\n" +
+  "  SERVICE wikibase:around {\n" +
+  "    ?place wdt:P625 ?location .\n" +
+  "    bd:serviceParam wikibase:center ?flensburgLoc .\n" +
+  "    bd:serviceParam wikibase:radius \"+"+$.urlParam('km')+"\" .\n" +
+  "  } .\n" +
+  "  # Is an school\n" +
+  "  FILTER EXISTS { ?place wdt:P31 wd:Q159334 } .\n" +
+  "  OPTIONAL{?place wdt:P6375 ?address}.   \n" +
+  "  OPTIONAL{?place wdt:P2196 ?schueler}.   \n" +
+  "  SERVICE wikibase:label {\n" +
+  "    bd:serviceParam wikibase:language \"de\" .\n" +
+  "  } \n" +
+  "}";    
+makeSPARQLQuery( endpointUrl, sparqlQuery, function( data ) {
+    data.results.bindings.forEach(element => {
+    $("form").css("display", "none");
 
-const queryDispatcher = new SPARQLQueryDispatcher( endpointUrl );
-queryDispatcher.query( sparqlQuery ).then( (result)=>{
-  console.log(result.results.bindings)
-  result.results.bindings.forEach((item)=>{
-    console.log(item)
-
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(item.placeLabel.value);
-    node.appendChild(textnode);
-    document.getElementById("myList").appendChild(node)
-  })
-});
+      $( 'body' ).append( $( '<div class="label">' ).html( 
+        '<div class="sender"> <div class="name">Chaostreff Flensburg e.V.</div>'+
+        "Apenrader Str. 49, 24937 Flensburg".replace(',', '<br/>') +
+        '</div>'+
+        '<div class="empfaenger">'+
+        '<span class="name">'+
+        element.placeLabel.value + 
+        '</span>'+
+        element.address.value.replace(',', '<br/>') +
+        '</div>'
+      ));
+    });
+    }
+);
+  }
